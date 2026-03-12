8cedbcaf8071ccf06d6ec9e92959c31fa6f9db3c
Akdev Saha <akdevsaha2003@gmail.com>
2026-03-12 17:55:34 +0530

    migrations

---
+++ backend/drizzle/0001_careless_odin.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "chats" ADD COLUMN "created_by" uuid NOT NULL;--> statement-breakpoint
+ALTER TABLE "chats" ADD CONSTRAINT "chats_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
